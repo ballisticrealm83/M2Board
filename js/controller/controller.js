b2c1b68e74ac06f5d7067fe73d7f130ef65391ff
ballistic_realm <ballistic_realm@yahoo.co.uk>
2016-09-07 15:39:46 -0400

    angular include

---
+++ js/controller/controller.js
@@ -0,0 +1,9 @@
+﻿angular.module('m2Board', [])
+
+    .controller('gaugeController', ['$scope', function GaugeController($scope) {
+        $scope.contacts = ["abcd@gmail.com", "abcd@yahoo.co.in"];
+       
+    }]);
+
+
+
