c73126c336982eb7cbb5f1c9f31d7f902c5940f7
Mengdi-Melanie <hmdmelanie@gmail.com>
2016-09-07 23:26:33 -0400

    hmd

--- js/controller/lineChartController.js
+++ js/controller/lineChartController.js
@@ -1,1 +1,20 @@
-﻿
+﻿(function () {
+
+
+    angular.module('m2Board', [])
+
+    .controller('lineChartController', ['$scope', 'currencyService', function lineChartController($scope, currencyService) {
+
+        alert("Melanie");
+
+
+        function loadLineChart(currency) {
+            alert("Melanie2");
+        }
+
+
+
+    }]);
+
+
+}());
