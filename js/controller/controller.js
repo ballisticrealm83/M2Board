aeb976789a5206dd640bcfa1bd4da9ee42f4f100
ballistic_realm <ballistic_realm@yahoo.co.uk>
2016-09-07 23:24:41 -0400

    Signed-off-by: ballistic_realm <ballistic_realm@yahoo.co.uk>

--- js/controller/controller.js
+++ js/controller/controller.js
@@ -5,10 +5,21 @@
 
     .controller('gaugeController', ['$scope', 'currencyService', function GaugeControler($scope, currencyService) {
 
-        alert(currencyService.getCurrencies()['SGD']);
+        //alert(currencyService.getCurrencies()['SGD']);
 
 
+        var balance = currencyService.getCurrencyBalance()['USD'];
+       
+        var percentage = balance / 100;
 
+        var greenArcMaskStroke = percentage * 235;
+        var greenArcStroke = 235 - greenArcMaskStroke;
+
+        //alert(greenArcStroke);
+        //alert(greenArcMaskStroke);
+
+        $("#redArc").attr("stroke-dasharray", greenArcStroke + ",943");
+        $("#redArcMask").attr("stroke-dasharray", greenArcMaskStroke+",943");
 
         /*
         setTimeout(
